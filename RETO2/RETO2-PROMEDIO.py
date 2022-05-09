07c7a44eebf249d23b3a92b3ef8f62a9fff52089
Sergio Córdoba <89363748+sergiocordobam@users.noreply.github.com>
2022-05-09 18:54:12 -0500

    Update RETO2-PROMEDIO.py

--- RETO2/RETO2-PROMEDIO.py
+++ RETO2/RETO2-PROMEDIO.py
@@ -1,16 +1,16 @@
 def calcular_promedio_y_cuadro_de_honor(grupo: list):
     sumaNotas = 0 
     cuadro_honor = {"1": [], "2": [], "3": [] }
-    for i in grupo:
-        for k,v in i.items():
-            sumaNotas += i["nota_fundamentos"]
-            if i["nota_fundamentos"] == 5:
-                cuadro_honor["1"].append(i["cédula"])
-            elif i["nota_fundamentos"] >= 4 and i["nota_fundamentos"] < 5:
-                cuadro_honor["2"].append(i["cédula"])
+    for estudiante in grupo:
+        for key,value in estudiante.items():
+            sumaNotas += estudiante["nota_fundamentos"]
+            if estudiante["nota_fundamentos"] == 5:
+                cuadro_honor["1"].append(estudiante["cédula"])
+            elif estudiante["nota_fundamentos"] >= 4 and estudiante["nota_fundamentos"] < 5:
+                cuadro_honor["2"].append(estudiante["cédula"])
             else:
-                if i["nota_fundamentos"] >= 3 and i["nota_fundamentos"] < 4:
-                    cuadro_honor["3"].append(i["cédula"])
+                if estudiante["nota_fundamentos"] >= 3 and estudiante["nota_fundamentos"] < 4:
+                    cuadro_honor["3"].append(estudiante["cédula"])
             break
     promedio = (sumaNotas/len(grupo))
     return promedio, cuadro_honor
@@ -44,3 +44,4 @@ grupo = [estudiante1, estudiante2, estudiante3, estudiante4]
 print(calcular_promedio_y_cuadro_de_honor(grupo))
 
     
+
